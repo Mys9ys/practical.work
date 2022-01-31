80e72fde401e02d2a534d0be4aecea3bc690b43c
Mys9ys9ka <rjbexa@yandex.ru>
2022-01-31 13:35:06 +0300

    Профессиональной чистке ковров

---
+++ news_item.php
@@ -0,0 +1,18 @@
+Всё, что вы не знали о..?
+
+<p>..Профессиональной чистке ковров.</p>
+
+<h3>..Профессиональной чистке ковров.</h3><br>
+
+<p>Приветствуем вас, друзья!</p>
+
+<p>Совсем скоро сеть химчисток-прачечных «Диана» приоткроет вам секреты работы ковровой фабрики и покажет полный
+    технологический цикл, через который проходит каждое изделие, попадая в руки к нашим технологам. </p>
+<p>Обещаем, будет интересно! Следите за новостями!</p>
+
+https://dryclean.ru/upload/medialibrary/2a3/05vibmqsagpnygwj9mx5sj0ihe1ssnga/n_20_01_22_3.jpeg
+
+https://dryclean.ru/upload/medialibrary/870/l7d3uz6ty1c2sw1kxpz3aa7lz9n0m9fd/n_20_01_22_2.jpeg
+
+<div style="background-image: url('/upload/medialibrary/2a3/05vibmqsagpnygwj9mx5sj0ihe1ssnga/n_20_01_22_3.jpeg'); background-repeat: no-repeat; max-width: 800px; height: 533px; margin: 20px auto;"></div>
+<div style="background-image: url('/upload/medialibrary/870/l7d3uz6ty1c2sw1kxpz3aa7lz9n0m9fd/n_20_01_22_2.jpeg'); background-repeat: no-repeat; max-width: 800px; height: 533px; margin: 20px auto;"></div>
