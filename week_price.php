e0af225c5d06faa453c1955206e376629af303c4
mys9ys <rjbexa@yandex.ru>
2022-05-11 10:52:09 +0300

    merge commit 11-05

--- week_price.php
+++ week_price.php
@@ -7,11 +7,6 @@
 <p>
 
 
-
-
-
-
-
 </p>
 
 <p>
@@ -27,7 +22,9 @@
 </ul>
 
 <ul>
-    <li>Ковер низковорсный без сложного декора, без подкладки, кроме вискозных и шелковых, эксклюзивных и мокрых ковровых изделий, цена за метр кв.</li>
+    <li>Ковер низковорсный без сложного декора, без подкладки, кроме вискозных и шелковых, эксклюзивных и мокрых
+        ковровых изделий, цена за метр кв.
+    </li>
     <li>Шторы плотные (портьеры)</li>
 </ul>
 
@@ -52,3 +49,38 @@
     <li>Пальто, полупальто, комбинезон утепленные, в т. ч. на пуху с отделкой из натурального меха;</li>
     <li>Водоотталкивающая пропитка пальто.</li>
 </ul>
+
+
+<td align="center" id="mmt7-7" valign="top"
+    style="width: 20px; height: 20px; background-color: silver; border-width: 2px 1px 1px 2px; border-style: solid; border-color: black silver silver black;"
+    title=""><span style="font-size: 5pt;" id="mmc7-7">32:61</span><br><span id="mmd7-7"><font size="-2"><b>E</b></font></span>
+</td>
+
+<td align="center" id="mmt7-7" valign="top"
+    style="width: 20px; height: 20px; background-color: silver; border-width: 2px 2px 2px 1px; border-style: solid; border-color: black black black silver;"
+    title=""><span style="font-size: 5pt;" id="mmc7-7">33:63</span><br><span id="mmd7-7"></span></td>
+
+
+<td align="center" id="mmt7-7" valign="top"
+    style="width: 20px; height: 20px; background-color: silver; border-width: 1px 2px 2px 1px; border-style: solid; border-color: silver black black silver;"
+    title=""><span style="font-size: 5pt;" id="mmc7-7">38:6</span><br><span id="mmd7-7"></span></td>
+
+<td align="center" id="mmt7-7" valign="top"
+    style="width: 20px; height: 20px; background-color: silver; border-width: 2px 2px 1px 1px; border-style: solid; border-color: black black silver silver;"
+    title=""><span style="font-size: 5pt;" id="mmc7-7">38:7</span><br><span id="mmd7-7"></span></td>
+
+
+<p>
+    Ролик для чистки одежды убирает ворсинки, нитки, пух, шерсть животных, мелкие крошки и обрывки бумаги.
+    Рекомендуется использовать при очищении одежды, мебели, салонов автомобилей и других изделий из любых видов ткани.
+    Эргономичная ручка из качественного пластика удобна в использовании: </p>
+    <ul>
+    <li>не выскальзывает из руки;</li>
+    <li> не разбалтывается.</li>
+</ul>
+<p>
+    Липкий слой убирает загрязнения, при этом не оставляя следов на поверхности материала. Диагональная намотка
+    позволяет одним движением удалять использованный слой. В ролике 60 листов.
+</p>
+
+<img class="before-after__item twentytwenty-before" src="/local/components/diana/service_page.after_before/templates/.default/additional/img/mat_garb.jpg" style="clip: rect(0px, 283.5px, 319px, 0px);">
