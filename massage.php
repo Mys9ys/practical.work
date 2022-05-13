43a2a6681bd4f33bba21462a191c08310124a6cb
mys9ys <rjbexa@yandex.ru>
2022-05-13 11:25:00 +0300

    massage orin

---
+++ massage.php
@@ -0,0 +1,244 @@
+<div class="x-grid3-scroller" id="ext-gen19" style="overflow: visible; position: static;">
+    <div class="x-grid3-body" style="width: 1302px;" id="ext-gen21">
+        <div class="x-grid3-row  x-grid3-row-first" style="width:1302px;">
+            <table class="x-grid3-row-table" border="0" cellspacing="0" cellpadding="0" style="width:1302px;">
+                <tbody>
+                <tr>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-0 x-selectable x-grid3-cell-first "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-0">Шейно-воротниковая зона 20 минут</div>
+                    </td>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-1 x-selectable x-grid3-cell-last "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-1">800</div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="x-grid3-row x-grid3-row-alt" style="width:1302px;">
+            <table class="x-grid3-row-table" border="0" cellspacing="0" cellpadding="0" style="width:1302px;">
+                <tbody>
+                <tr>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-0 x-selectable x-grid3-cell-first "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-0">Шейно-грудной отдел 30 минут</div>
+                    </td>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-1 x-selectable x-grid3-cell-last "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-1">1 300</div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="x-grid3-row " style="width:1302px;">
+            <table class="x-grid3-row-table" border="0" cellspacing="0" cellpadding="0" style="width:1302px;">
+                <tbody>
+                <tr>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-0 x-selectable x-grid3-cell-first "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-0">Пояснично-крестцовый отдел 20 минут</div>
+                    </td>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-1 x-selectable x-grid3-cell-last "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-1">800</div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="x-grid3-row x-grid3-row-alt" style="width:1302px;">
+            <table class="x-grid3-row-table" border="0" cellspacing="0" cellpadding="0" style="width:1302px;">
+                <tbody>
+                <tr>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-0 x-selectable x-grid3-cell-first "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-0">Массаж живота 15 минут</div>
+                    </td>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-1 x-selectable x-grid3-cell-last "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-1">500</div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="x-grid3-row " style="width:1302px;">
+            <table class="x-grid3-row-table" border="0" cellspacing="0" cellpadding="0" style="width:1302px;">
+                <tbody>
+                <tr>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-0 x-selectable x-grid3-cell-first "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-0">Массаж спины общий 40 минут</div>
+                    </td>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-1 x-selectable x-grid3-cell-last "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-1">1 500</div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="x-grid3-row x-grid3-row-alt" style="width:1302px;">
+            <table class="x-grid3-row-table" border="0" cellspacing="0" cellpadding="0" style="width:1302px;">
+                <tbody>
+                <tr>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-0 x-selectable x-grid3-cell-first "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-0">Верхние конечности (х2) 30 минут</div>
+                    </td>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-1 x-selectable x-grid3-cell-last "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-1">1 400</div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="x-grid3-row " style="width:1302px;">
+            <table class="x-grid3-row-table" border="0" cellspacing="0" cellpadding="0" style="width:1302px;">
+                <tbody>
+                <tr>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-0 x-selectable x-grid3-cell-first "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-0">Нижние конечности (х2) 30 минут</div>
+                    </td>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-1 x-selectable x-grid3-cell-last "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-1">1 600</div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="x-grid3-row x-grid3-row-alt" style="width:1302px;">
+            <table class="x-grid3-row-table" border="0" cellspacing="0" cellpadding="0" style="width:1302px;">
+                <tbody>
+                <tr>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-0 x-selectable x-grid3-cell-first "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-0">Массаж предплечье + кисти (х2) 20 минут</div>
+                    </td>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-1 x-selectable x-grid3-cell-last "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-1">1 000</div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="x-grid3-row " style="width:1302px;">
+            <table class="x-grid3-row-table" border="0" cellspacing="0" cellpadding="0" style="width:1302px;">
+                <tbody>
+                <tr>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-0 x-selectable x-grid3-cell-first "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-0">Массаж предплечье + кисть 10 минут</div>
+                    </td>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-1 x-selectable x-grid3-cell-last "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-1">500</div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="x-grid3-row x-grid3-row-alt" style="width:1302px;">
+            <table class="x-grid3-row-table" border="0" cellspacing="0" cellpadding="0" style="width:1302px;">
+                <tbody>
+                <tr>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-0 x-selectable x-grid3-cell-first "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-0">Массаж голени + стопы (х2) 25 минут</div>
+                    </td>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-1 x-selectable x-grid3-cell-last "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-1">1 000</div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="x-grid3-row " style="width:1302px;">
+            <table class="x-grid3-row-table" border="0" cellspacing="0" cellpadding="0" style="width:1302px;">
+                <tbody>
+                <tr>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-0 x-selectable x-grid3-cell-first "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-0">Массаж голени + стопа 15 минут</div>
+                    </td>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-1 x-selectable x-grid3-cell-last "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-1">500</div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="x-grid3-row x-grid3-row-alt" style="width:1302px;">
+            <table class="x-grid3-row-table" border="0" cellspacing="0" cellpadding="0" style="width:1302px;">
+                <tbody>
+                <tr>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-0 x-selectable x-grid3-cell-first "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-0">Массаж стоп (х2) 15 минут</div>
+                    </td>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-1 x-selectable x-grid3-cell-last "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-1">400</div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="x-grid3-row " style="width:1302px;">
+            <table class="x-grid3-row-table" border="0" cellspacing="0" cellpadding="0" style="width:1302px;">
+                <tbody>
+                <tr>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-0 x-selectable x-grid3-cell-first "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-0">Верхние и нижние конечности 30 минут</div>
+                    </td>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-1 x-selectable x-grid3-cell-last "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-1">1 500</div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="x-grid3-row x-grid3-row-alt" style="width:1302px;">
+            <table class="x-grid3-row-table" border="0" cellspacing="0" cellpadding="0" style="width:1302px;">
+                <tbody>
+                <tr>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-0 x-selectable x-grid3-cell-first "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-0">Массаж крупного сустава 15 минут</div>
+                    </td>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-1 x-selectable x-grid3-cell-last "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-1">300</div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="x-grid3-row  x-grid3-row-last" style="width:1302px;">
+            <table class="x-grid3-row-table" border="0" cellspacing="0" cellpadding="0" style="width:1302px;">
+                <tbody>
+                <tr>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-0 x-selectable x-grid3-cell-first "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-0">Общий массаж 60 минут</div>
+                    </td>
+                    <td class="x-grid3-col x-grid3-cell x-grid3-td-1 x-selectable x-grid3-cell-last "
+                        style="width: 649px;" tabindex="0">
+                        <div class="x-grid3-cell-inner x-grid3-col-1">3 500</div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <a href="#" class="x-grid3-focus" tabindex="-1" id="ext-gen22" style="left: 1px; top: 636px;"></a></div>
