3e5d1bdbd1eda5a00c14c2f98f8fd82561f29c35
Mys9ys9ka <rjbexa@yandex.ru>
2022-05-15 22:18:01 +0300

    orin upgrade price box

---
+++ orin_price_box_temp.php
@@ -0,0 +1,10 @@
+<table class="service-table">
+    <thead>
+    <tr><th>Наименование услуги</th><th style="text-align: right;">Цена, (руб.)</th></tr>
+    </thead>
+    <tbody>
+
+
+
+    </tbody>
+</table>
