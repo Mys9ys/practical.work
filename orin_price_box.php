7a3c1c83267fdd3464aecc7ee1efcbe596ae64c5
mys9ys <rjbexa@yandex.ru>
2022-05-26 16:05:02 +0300

    massage orin+

--- orin_price_box.php
+++ orin_price_box.php
@@ -4,54 +4,18 @@
     </thead>
     <tbody>
 
-    <tr><td>Прием врача - оториноларинголога первичный</td><td>1700</td></tr>
-    <tr><td>Прием врача оториноларинголога повторный (в теч. 14 дней)</td><td>1500</td></tr>
-    <tr><td>Прием оториноларинголога Жук Е.Г.</td><td>1700</td></tr>
-    <tr><td>Анемизация слизистой полости носа</td><td>200</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Анестезия апликационная (лор)</td><td>200</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Введение лекарств в наружный слуховой проход</td><td>200</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Введение лекарственных средств в околоносовые пазухи</td><td>200</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Исследование функций слуховой трубы</td><td>200</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Орошение лекарственными препаратами глотки</td><td>200</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Орошение лекарственными препаратами носа</td><td>200</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Смена наружной асептической повязки</td><td>200</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Анестезия инфильтрационная (лор)</td><td>300</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Введение лекарственных препаратов через катетер</td><td>300</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Вращательная проба (исследов. вестибул. апарата в кресле Бараньи)</td><td>300</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Пневмомассаж барабанных перепонок аппаратный (1 ухо)</td><td>300</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Транстимпанальное введение лекарственных средств</td><td>300</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Турунда в ЛОР органы с лекарственным веществом</td><td>300</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Катетеризация слуховой трубы -1 сторона</td><td>400</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Продувание ушей по Политцеру</td><td>400</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Туалет полости носа</td><td>400</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Туалет уха (одно ухо)</td><td>400</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Внутрираковинная блокада</td><td>500</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Исследование биноурального слуха камертонами и шепотной речью</td><td>500</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Промывание оперированной верхнечелюстной, лобной, клиновидной пазух через соустья - 1 сторона</td><td>500</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Прижигание задней стенки глотки (медикаментозное)</td><td>600</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Прижигание кровоточащего сосуда носа (медикаментозное)</td><td>600</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Промывание уха при грибковых и других заболеваниях (одно ухо)</td><td>600</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Эндоларингологическое (внутригортанное вливание) введение лекарственных веществ</td><td>600</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Удаление инородного тела из носа</td><td>700</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Удаление инородного тела из ротоглотки</td><td>700</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Тимпанометрия</td><td>750</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Удаление серной пробки (одна сторона)</td><td>750</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Удаление серной пробки инструментальное (одна сторона)</td><td>750</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Удаление инородного тела из наружного слухового прохода</td><td>800</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Промывание оперированной верхнечелюстной, лобной, клиновидной пазухи через соустья - 2 стороны</td><td>1000</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Удаление инородного тела из гортани</td><td>1000</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Промывание миндалин лекарственными средствами</td><td>1100</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Промывание полости носа методом перемещения «кукушка»</td><td>1100</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Пункция верхнечелюстной пазухи - 1 сторона</td><td>1200</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Эндоскопическое исследование</td><td>1200</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Удаление серной пробки (две стороны)</td><td>1250</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Прижигание кровоточащего сосуда в полости носа (аппаратное)</td><td>1500</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Передняя тампонада носа</td><td>1600</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Вскрытие фурункула, абсцесса, атеромы, гематомы ЛОР органов</td><td>2000</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Пункция верхнечелюстной пазухи - 2 стороны</td><td>2000</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Вазотомия нижних носовых раковин - 1 сторона</td><td>5000</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Вазотомия нижних носовых раковин - 2 сторон</td><td>9000</td></tr>
-
+    <tr><td>Электрокардиография (ЭКГ) без расшифровки</td><td>800</td></tr>
+    <tr><td>Электрокардиография (ЭКГ) с расшифровкой</td><td>1200</td></tr>
+    <tr><td>ЭКГ в трех позициях</td><td>1600</td></tr>
+    <tr><td>ЭКГ с нагрузкой</td><td>1600</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ сосудов (артерий) верхних конечностей</td><td>2400</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ сосудов (артерий) нижних конечностей</td><td>2400</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ сосудов (вен) верхних конечностей</td><td>2400</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ сосудов (вен) нижних конечностей</td><td>2400</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Ультразвуковое доплеровское сканирование магистральных сосудов шеи (УЗДГ сосудов шеи)</td><td>2400</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Эхокардиография (УЗИ сердца, ЭХО-КГ)</td><td>2450</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Суточное мониторирование артериального давления</td><td>2500</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Холтеровское мониторирование ЭКГ</td><td>2500</td></tr>
 
     </tbody>
 </table>
--- orin_price_box_temp.php
+++ orin_price_box_temp.php
@@ -4,6 +4,53 @@
     </thead>
     <tbody>
 
+    <tr><td>Прием детского хирурга</td><td>2000</td></tr>
+    <tr><td>Прием проктолога</td><td>2000</td></tr>
+    <tr><td>Прием сосудистого хирурга-флеболога</td><td>2000</td></tr>
+    <tr><td>Прием хирурга</td><td>2000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Наложение фиксирующей повязки</td><td>500</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Снятие гипсовых лангет</td><td>500</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Аппликационная анестезия в хирургии</td><td>600</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Местная анестезия в хирургии</td><td>700</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Наложение малой гипсовой повязки</td><td>1000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Осмотр хирурга перед удалением</td><td>1000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Снятие послеоперационных швов</td><td>1000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Аноскопия</td><td>1200</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Удаление доброкачественных образований аппаратом Фотек 1 мм</td><td>1200</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Биопсия новообразований щипковая для гистологического исследования</td><td>1500</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Инфильтрационная анестезия в хирургии</td><td>1500</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Наложение повязки при нарушении целостности кожных покровов в хирургии</td><td>1500</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Проводниковая анестезия в хирургии</td><td>1500</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Диагностическая пункция мягких тканей (без стоимости цитологического исследования)</td><td>2000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Диагностическая пункция суставов</td><td>2000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Пункция мелкого сустава</td><td>2000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Удаление доброкачественных образований аппаратом Фотек до 3 мм</td><td>2000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Хирургическая перевязка</td><td>2000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Наложение повязки при гнойных заболеваниях кожи и подкожной клетчатки в хирургии</td><td>2250</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Удаление подошвенной бородавки, сухой мозоли</td><td>2250</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Наложение большой гипсовой повязки</td><td>2500</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Вскрытие панариция</td><td>2750</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Удаление доброкачественных образований аппаратом Фотек до 5 мм</td><td>2800</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Вскрытие и дренирование абсцесса мягких тканей с наложением лечебной повязки</td><td>3000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Краевая резекция ногтевой пластины</td><td>3200</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Вскрытие абсцедирующего фурункула (перианальной области промежности)</td><td>3250</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Вскрытие гематомы</td><td>3250</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Пункция тазобедренного сустава</td><td>3250</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Вскрытие гидраденита</td><td>3750</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Вскрытие карбункула</td><td>3750</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Обработка осложнённой раны с удалением ногтевой пластинки</td><td>4000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Ректороманоскопия</td><td>4000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Краевая резекция ногтевой пластины - радиохирургическая</td><td>4350</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Паравертебральная блокада с Дипроспаном</td><td>5000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Пластика мягких тканей</td><td>5000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Удаление множественных мелких доброкачественных образований аппаратом Фотек (1 зона, до 5 образований)</td><td>5000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Удаление доброкачественных новообразований кожи размерами до 1 см с гистологическим исследованием полученного материала</td><td>5250</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Пункция мягких тканей под контролем ультразвукового исследования с цитологическим исследованием полученного материала в хирургии</td><td>5700</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Удаление инородного тела с рассечением мягких тканей</td><td>5900</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Вскрытие подкожно-подслизистого парапроктита (операция, включающая вскрытие гнойника, кюретаж, туалет раны, гемостаз и постановку дренажей, без учета стоимости последующих перевязок)</td><td>7000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Тромбэктомия геммороидального узла (операция с последующим прошиванием сосудистой ножки, без учета стоимости последующих перевязок)</td><td>7400</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Удаление доброкачественных новообразований кожи размерами более 1 см с гистологическим исследованием полученного материала</td><td>7500</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Удаление доброкачественных новообразований более 5 см</td><td>15200</td></tr>
 
 
     </tbody>
