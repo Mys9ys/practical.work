09c840b1839f84966755f9978b21d7f4f878283c
Mys9ys9ka <rjbexa@yandex.ru>
2022-07-08 12:31:42 +0300

    orin узи цены

--- orin_price_box.php
+++ orin_price_box.php
@@ -4,53 +4,45 @@
     </thead>
     <tbody>
 
-    <tr><td>Прием врача - оториноларинголога первичный</td><td>1700</td></tr>
-    <tr><td>Прием врача оториноларинголога повторный (в теч. 14 дней)</td><td>1500</td></tr>
-    <tr><td>Прием оториноларинголога Жук Е.Г.</td><td>1700</td></tr>
-    <tr><td>Анемизация слизистой полости носа</td><td>200</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Анестезия апликационная (лор)</td><td>200</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Введение лекарств в наружный слуховой проход</td><td>200</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Введение лекарственных средств в околоносовые пазухи</td><td>200</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Исследование функций слуховой трубы</td><td>200</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Орошение лекарственными препаратами глотки</td><td>200</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Орошение лекарственными препаратами носа</td><td>200</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Смена наружной асептической повязки</td><td>200</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Анестезия инфильтрационная (лор)</td><td>300</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Введение лекарственных препаратов через катетер</td><td>300</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Вращательная проба (исследов. вестибул. апарата в кресле Бараньи)</td><td>300</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Пневмомассаж барабанных перепонок аппаратный (1 ухо)</td><td>300</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Транстимпанальное введение лекарственных средств</td><td>300</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Турунда в ЛОР органы с лекарственным веществом</td><td>300</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Катетеризация слуховой трубы -1 сторона</td><td>400</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Продувание ушей по Политцеру</td><td>400</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Туалет полости носа</td><td>400</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Туалет уха (одно ухо)</td><td>400</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Внутрираковинная блокада</td><td>500</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Исследование биноурального слуха камертонами и шепотной речью</td><td>500</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Промывание оперированной верхнечелюстной, лобной, клиновидной пазух через соустья - 1 сторона</td><td>500</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Прижигание задней стенки глотки (медикаментозное)</td><td>600</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Прижигание кровоточащего сосуда носа (медикаментозное)</td><td>600</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Промывание уха при грибковых и других заболеваниях (одно ухо)</td><td>600</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Эндоларингологическое (внутригортанное вливание) введение лекарственных веществ</td><td>600</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Удаление инородного тела из носа</td><td>700</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Удаление инородного тела из ротоглотки</td><td>700</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Тимпанометрия</td><td>750</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Удаление серной пробки (одна сторона)</td><td>750</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Удаление серной пробки инструментальное (одна сторона)</td><td>750</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Удаление инородного тела из наружного слухового прохода</td><td>800</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Промывание оперированной верхнечелюстной, лобной, клиновидной пазухи через соустья - 2 стороны</td><td>1000</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Удаление инородного тела из гортани</td><td>1000</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Промывание миндалин лекарственными средствами</td><td>1100</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Промывание полости носа методом перемещения «кукушка»</td><td>1100</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Пункция верхнечелюстной пазухи - 1 сторона</td><td>1200</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Эндоскопическое исследование</td><td>1200</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Удаление серной пробки (две стороны)</td><td>1250</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Прижигание кровоточащего сосуда в полости носа (аппаратное)</td><td>1500</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Передняя тампонада носа</td><td>1600</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Вскрытие фурункула, абсцесса, атеромы, гематомы ЛОР органов</td><td>2000</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Пункция верхнечелюстной пазухи - 2 стороны</td><td>2000</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Вазотомия нижних носовых раковин - 1 сторона</td><td>5000</td></tr>
-    <tr class='td-hide' wfd-invisible='true'><td>Вазотомия нижних носовых раковин - 2 сторон</td><td>9000</td></tr>
+    <tr><td>УЗИ лимфатических узлов 1-2 регионов</td><td>650</td></tr>
+    <tr><td>УЗИ мочевого пузыря</td><td>650</td></tr>
+    <tr><td>УЗИ плевральных полостей</td><td>1000</td></tr>
+    <tr><td>Ультразвуковой мониторинг созревания фолликулов - фолликулометрия</td><td>1050</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Цервикометрия (УЗИ шейки матки)</td><td>1100</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ щитовидной железы, паращитовидных желез, околощитовидных лимфатических узлов</td><td>1200</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Нейросонография</td><td>1300</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ малого срока беременности до 7 недель(медикаментозный)</td><td>1350</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ молочных желез</td><td>1350</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ малого таза у женщин (трансабдоминально)</td><td>1450</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Доплерометрия плода</td><td>1500</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Триплексное сканирование почечных артерий и вен</td><td>1500</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Триплексное сканирование чревного ствола</td><td>1500</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ малого таза (исключение жидкостных образований)</td><td>1500</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ органов мошонки</td><td>1500</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ почек, надпочечников и забрюшинного пpостpанства</td><td>1500</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ предстательной железы трансабдоминальное</td><td>1500</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ таза у мужчин (контроль после оперативного лечения)</td><td>1500</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>I триместр (УЗИ 1-13 недель)</td><td>1600</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ малого таза у женщин (трансвагинально) ТВУЗИ</td><td>1600</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ 11-14 неделя (скрининг)</td><td>1700</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ органов брюшной полости (печень, желчный пузырь, желчные протоки, поджелудочная железа, селезенка)</td><td>1700</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ 7-10 недель (многоплодная беременность)</td><td>1800</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ 11-14 скрининг (многоплодная беременность)</td><td>1900</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Доплерометрия плода (многоплодная беременность)</td><td>2000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Тонкоигольная аспирационная биопсия Щитовидной железы под УЗИ контролем</td><td>2000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Трансректальное ультразвуковое исследование ТРУЗИ (предстательная железа, семенные пузырьки)</td><td>2000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Триплексное сканирование сосудов полового члена</td><td>2000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ брахиоцефальных артерий</td><td>2000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ матки и плода во II и III триместрах беременности</td><td>2000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ таза у мужчин</td><td>2000</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Триплексное сканирование брюшного отдела аорты и ее ветвей</td><td>2200</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ матки и плода во II и III триместрах беременности (многоплодная)</td><td>2300</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ органов брюшной полости с пробным завтраком (принести с собой сливки 20% жирности 100 мл)</td><td>2300</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Кардиотокография (КТГ) при многоплодной беременности</td><td>2500</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ органов брюшной полости + гастро-комплекс с водно-сифонной пробой</td><td>2500</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ органов брюшной полости + почки</td><td>2950</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>УЗИ брюшной полости (расширенное)</td><td>3500</td></tr>
+    <tr class='td-hide' wfd-invisible='true'><td>Триплексное сканирование сосудов полового члена (комплексное, с использованием медикаментов, без стоимости препарата)</td><td>3700</td></tr>
 
 
     </tbody>
